88ccb88eb5dccff3cf8e2ea471502b8dd3d8cfef
Ryan McDiarmid <ryan.mcdiarmid45@gmail.com>
2025-08-20 22:55:01 +0800

    update choice to use descriptions

--- coast-game/src/Components/scenBtn.js
+++ coast-game/src/Components/scenBtn.js
@@ -1,12 +1,12 @@
-export default function scenBtn ({decision, path, setPath}) {
+export default function scenBtn ({option, path, setPath}) {
     return (
         <button
             className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
             onClick={()=>{
-                setPath(path + decision.target)
+                setPath(path + option.target)
             }}
         >
-            {decision.option}
+            {option.desc}
         </button>
     )
 }
--- coast-game/src/Components/scenario.js
+++ coast-game/src/Components/scenario.js
@@ -7,8 +7,8 @@ export default function Scenario({scenarios, path, setPath}) {
         <div className="flex flex-col items-center gap-4">
             <em className="text-center text-lg">{scenario.text}</em>
             <div className="flex flex-col items-center gap-2">
-                {scenario.options.map((decision, i) => (
-                    <ScenBtn key={i} decision={decision} path={path} setPath={setPath} />
+                {scenario.options.map((option, i) => (
+                    <ScenBtn key={i} option={option} path={path} setPath={setPath} />
                 ))}
             </div>
         </div>
